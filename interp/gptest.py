0a33b42e5257304cb966def09a66ddc445c53c5f
Steven Reeves <reevessteven15@gmail.com>
2020-05-19 17:14:48 -0700

    Added python matrix handling

--- interp/gptest.py
+++ interp/gptest.py
@@ -1,10 +1,15 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
 import cv2
 from py_pipeline.weights import get_weights
+from py_pipeline.gp_conv_sr import gp_conv_sr
 from py_wrapper.srwrapper import GPupsamp
 
 filename = input('Enter a filename ')
 ratio = int(input('Enter an upsampling ratio: 2 or 4 '))
 img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-ks, c = get_weights(img.shape[0:2], ratio, 20.)
-img_out = GPupsamp(img, ratio, ks, c)
-cv2.imwrite('img_out.jpg', img_out)
+#ks, c = get_weights(img.shape[0:2], ratio, 20.)
+img_out = gp_conv_sr(img, ratio, 12.)
+
+#img_out = GPupsamp(img, ratio, ks, c)
+cv2.imwrite('img_out.png', img_out)
